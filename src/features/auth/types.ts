98a4cc36f2f3f6b03ebc6c01f95e502e46e21043
timohaBlr <94timoha@gmail.com>
2023-03-12 10:22:47 +0300

    fix: authReducer: статус логинизации привязан к булевым знчениям. Исправлен flow регистрации и сброса пароля

--- src/features/auth/types.ts
+++ src/features/auth/types.ts
@@ -2,7 +2,6 @@ import * as actions from './actions'
 import {InferValueTypes} from "../../app/types";
 
 export type AuthActionsType = ReturnType<InferValueTypes<typeof actions>>
-export type LoginTypes = 'unLoggedIn' | 'registered' | 'loggedIn'
 
 export type ResponseErrorDataType = {
     error: string | undefined
