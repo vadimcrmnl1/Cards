2413187e74230a9505f39b25e105a97d125a6404
vadimcrmnl1 <you@example.com>
2023-03-27 10:35:04 +0300

    add elsint, prettier

--- src/common/components/SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/common/components/SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -7,37 +7,32 @@ type DefaultInputPropsType = DetailedHTMLProps<
   HTMLInputElement
 >
 export type SuperDebouncedInputPropsType = Omit<DefaultInputPropsType, 'type'> & {
-    onChangeText?: (value: string) => void
-    onEnter?: () => void
-    error?: ReactNode
-    spanClassName?: string
-}
-    & {
-    onDebouncedChange?: (value: string) => void
+  onChangeText?: (value: string) => void
+  onEnter?: () => void
+  error?: ReactNode
+  spanClassName?: string
+} & {
+  onDebouncedChange?: (value: string) => void
 }
 
-export const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
-    {
-        onChangeText,
-        onDebouncedChange,
-        ...restProps
+export const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = ({
+  onChangeText,
+  onDebouncedChange,
+  ...restProps
+}) => {
+  const [timerId, setTimerId] = useState<number | undefined>(undefined)
+
+  const handleOnChangeText = (value: string) => {
+    onChangeText?.(value)
+    if (onDebouncedChange) {
+      clearTimeout(timerId)
+      setTimerId(
+        +setTimeout(() => {
+          onDebouncedChange(value)
+        }, 1500)
+      )
     }
-) => {
-    const [timerId, setTimerId] = useState<number | undefined>(undefined)
+  }
 
-    const handleOnChangeText = (value: string) => {
-        onChangeText?.(value)
-        if (onDebouncedChange) {
-            clearTimeout(timerId)
-            setTimerId(+(setTimeout(() => {
-                onDebouncedChange(value)
-            }, 1500)));
-        }
-    }
-    return (
-        <SuperInput onChangeText={handleOnChangeText}
-                    {...restProps}/>
-    )
+  return <SuperInput onChangeText={handleOnChangeText} {...restProps} />
 }
-
-
